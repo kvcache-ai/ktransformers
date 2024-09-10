5b7052865bddf593fc30b80f09082e5c22e19bce
chenht2022 <chenht2022@gmail.com>
2024-09-10 09:21:29 +0000

    fix backend

--- ktransformers/ktransformers_ext/cpu_backend/task_queue.cpp
+++ ktransformers/ktransformers_ext/cpu_backend/task_queue.cpp
@@ -4,7 +4,7 @@
  * @Date         : 2024-07-17 12:25:51
  * @Version      : 1.0.0
  * @LastEditors  : chenht2022
- * @LastEditTime : 2024-07-25 10:33:44
+ * @LastEditTime : 2024-09-10 09:20:27
  * @Copyright (c) 2024 by KVCache.AI, All Rights Reserved.
  **/
 #include "task_queue.h"
@@ -35,6 +35,7 @@ void TaskQueue::sync() {
 }
 
 void TaskQueue::processTasks() {
+    auto start = std::chrono::steady_clock::now();
     while (true) {
         mutex.lock();
         if (tasks.empty()) {
@@ -42,6 +43,11 @@ void TaskQueue::processTasks() {
                 return;
             }
             mutex.unlock();
+            auto now = std::chrono::steady_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
+            if (duration > 50) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
             continue;
         }
         std::function<void()> task = tasks.front();
@@ -53,5 +59,6 @@ void TaskQueue::processTasks() {
             sync_flag.store(true, std::memory_order_seq_cst);
         }
         mutex.unlock();
+        start = std::chrono::steady_clock::now();
     }
 }
