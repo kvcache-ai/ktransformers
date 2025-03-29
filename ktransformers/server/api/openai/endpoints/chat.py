61752061d0cde7ec77f0fa7f09b65a6e4c804af5
Creeper-MZ <e15503075646@hotmail.com>
2025-03-29 16:44:01 -0400

    Update chat.py

--- ktransformers/server/api/openai/endpoints/chat.py
+++ ktransformers/server/api/openai/endpoints/chat.py
@@ -72,16 +72,18 @@ async def chat_completion(request: Request, create: ChatCompletionCreate):
     enhanced_messages = list(create.messages)
     
     # 如果有工具，且第一条消息是system，在system提示中添加工具使用指导
-    if create.tools and len(create.tools) > 0 and (enhanced_messages[0].role == Role.system or enhanced_messages[0].role ==Role.user):
-        tool_instructions = "<functions>\n你可以使用以下工具：\n\n"
+    if create.tools and len(create.tools) > 0 and (enhanced_messages[0].role == Role.system or enhanced_messages[0].role == Role.user):
+        tool_instructions = "你可以使用以下工具，name为函数名称，description为函数功能的描述，parameters中含有函数需要使用的参数和参数的描述, 其中required为必要参数：\n\n"
         for tool in create.tools:
-            tool_instructions += f"name - {tool.function.name}: {tool.function.description} parameters: {tool.function.parameters}\n"
+            tool_instructions += f" \"function\":{{\"name\" : {tool.function.name},\"description\" : {tool.function.description} , \"parameters\" : {tool.function.parameters}}}\n"
         
         # 修改工具使用指南，鼓励JSON格式输出
         tool_instructions += "工具仅在用户明确提出，或者你认为需要调用工具的时候调用。当确实调用工具的关键信息时，你可以先向用户索取关键信息再调用工具\n"
         tool_instructions += "\n当你需要使用工具时，请以下列格式输出，格式为：\n"
         tool_instructions += '<｜tool▁calls▁begin｜><｜tool▁call▁begin｜>function<｜tool▁sep｜>name\n```json {"参数名": "参数值","参数名2": "参数值2"...}\n```<｜tool▁call▁end｜><｜tool▁calls▁end｜>\n'
-        tool_instructions += "不要尝试解释你在做什么，直接输出工具调用即可。</functions>"
+        tool_instructions += '示例: \n<｜tool▁calls▁begin｜><｜tool▁call▁begin｜>function<｜tool▁sep｜>the_functnion_name_will_be_called\n```json {"arg1": "value1","arg2": "value2"}\n```<｜tool▁call▁end｜><｜tool▁calls▁end｜>\n'
+        tool_instructions += "这样可以调用名为\"the_functnion_name_will_be_called\",并将value1和value2传入参数arg1,arg2\n"
+        tool_instructions += "不要尝试解释你在做什么，直接输出工具函数调用即可。"
         
         enhanced_messages[0].content = enhanced_messages[0].content + "\n\n" + tool_instructions
     
