9e597a25a86c497371ec6c188c462722b6a8769f
SkqLiao <SkqLiao@users.noreply.github.com>
2026-02-13 14:49:35 +0000

    deploy: 7d9943365ac69a398b4fe094a827291a65c361d6

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b0c7c361.js',
+                path_to_root + 'searchindex-926f92a4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
