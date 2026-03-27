2ca1008fe7e7ed20674826cb718909516846f791
yyj6666667 <yyj6666667@users.noreply.github.com>
2026-03-27 06:45:26 +0000

    deploy: 7a9daf0cd4d60d828b17ae667f6ec3a4ab0d9c7b

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1bf866f2.js',
+                path_to_root + 'searchindex-b4d4a2ba.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
