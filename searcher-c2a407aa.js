c3fa14f9c59e8b218c924e6cdd647002565366a4
ovowei <ovowei@users.noreply.github.com>
2026-04-18 13:30:33 +0000

    deploy: e327db58beac561d8376b600f97aa5f64887d67f

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f2f91d2d.js',
+                path_to_root + 'searchindex-b4d4a2ba.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
