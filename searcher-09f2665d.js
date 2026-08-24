96be9f560b05c4fb22f2615831ed90be11a9ac1a
JimmyPeilinLi <JimmyPeilinLi@users.noreply.github.com>
2026-08-24 18:46:32 +0000

    deploy: c40d37c63cb9b7d041c8489167b3b822961d12c5

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0d9a3ab5.js',
+                path_to_root + 'searchindex-e3313d7c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
